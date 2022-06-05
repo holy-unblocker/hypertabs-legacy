74f2180e942547d726fd4ca9402e80d659e94094
David Reed <x@sys32.dev>
2022-06-05 18:17:53 -0400

    remove link to games, fixes to quickly run on server

---
+++ uv/uv.config.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+{
+	function format(env) {
+		const { host, hostname, protocol } = location;
+
+		return env
+			.replace('%{location.host}', host)
+			.replace('%{location.hostname}', hostname)
+			.replace('%{location.protocol}', protocol);
+	}
+
+	self.__uv$config = {
+		prefix: '/uv/service/',
+		bare:
+			location.hostname === 'localhost'
+				? format('%{location.protocol}//%{location.hostname}:8001/')
+				: format('%{location.protocol}//uv.%{location.host}/'),
+		encodeUrl: Ultraviolet.codec.xor.encode,
+		decodeUrl: Ultraviolet.codec.xor.decode,
+		handler: '/uv/uv.handler.js',
+		bundle: '/uv/uv.bundle.js',
+		config: '/uv/uv.config.js',
+		sw: '/uv/uv.sw.js',
+	};
+}
