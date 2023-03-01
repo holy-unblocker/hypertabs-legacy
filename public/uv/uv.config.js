c6db51fee43ea1947edcf1baf27047a2a0e42729
David Reed <reedswork@proton.me>
2023-03-01 18:24:13 -0500

    upgrade UV

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,25 +1,26 @@
 /* eslint-disable */
 {
-	function format(env) {
-		const { host, hostname, protocol } = location;
+  function format(env) {
+    const { host, hostname, protocol } = location;
 
-		return env
-			.replace('%{location.host}', host)
-			.replace('%{location.hostname}', hostname)
-			.replace('%{location.protocol}', protocol);
-	}
+    return env
+      .replace("%{location.host}", host)
+      .replace("%{location.hostname}", hostname)
+      .replace("%{location.protocol}", protocol);
+  }
 
-	self.__uv$config = {
-		prefix: '/uv/service/',
-		bare:
-			location.hostname === 'localhost'
-				? format('%{location.protocol}//%{location.hostname}:8001/')
-				: format('%{location.protocol}//uv.%{location.host}/'),
-		encodeUrl: Ultraviolet.codec.xor.encode,
-		decodeUrl: Ultraviolet.codec.xor.decode,
-		handler: '/uv/uv.handler.js',
-		bundle: '/uv/uv.bundle.js',
-		config: '/uv/uv.config.js',
-		sw: '/uv/uv.sw.js',
-	};
+  self.__uv$config = {
+    prefix: "/uv/service/",
+    bare:
+      location.hostname === "localhost"
+        ? format("%{location.protocol}//%{location.hostname}:8001/")
+        : format("%{location.protocol}//uv.%{location.host}/"),
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: "/uv/uv.handler.js",
+    bundle: "/uv/uv.bundle.js",
+    config: "/uv/uv.config.js",
+    client: "/uv/uv.client.js",
+    sw: "/uv/uv.sw.js",
+  };
 }
